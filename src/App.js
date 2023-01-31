9b857ee4f6fca37bcc79eaf27ef0e73c2798183f
zhannawebart <ms.zhannavasileva@gmail.com>
2023-01-31 14:11:21 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from './data';
+import nextBtn from './next.png';
+import previousBtn from './previous.png';
 import './App.css';
 
 function App() {
+
+  const [firstPlace, setPlace] = useState(0);
+  const {id, image, name, description} = data[firstPlace];
+  //console.log(africa[firstPlace]);
+
+  const nextPicture = () => {
+    setPlace((firstPlace => {
+      firstPlace ++;
+      if (firstPlace > data.length -1) {
+        firstPlace = 0;
+      }
+      return firstPlace;
+    }))
+  }
+
+  const previousPicture = () => {
+    setPlace((firstPlace => {
+      firstPlace --;
+      if (firstPlace < 0) {
+        return data.length -1;
+      }
+      return firstPlace;
+    }))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container">
+        <h1>Best places in Africa</h1>
+        <h2>Akuna matata:)</h2>
+        <div className="pictures-carousel">
+          <button onClick={previousPicture} className="carousel-btn"><img src={previousBtn} width="30px" alt="arrow"/></button>
+          <img src={image} width="400px" alt="zanzibar"/>
+          <button onClick={nextPicture} className="carousel-btn"><img src={nextBtn} width="30px" alt="arrow"/></button>
+        </div>
+        <h3>{id}. {name}</h3>
+        <div className="textBox">
+          <p>{description}</p>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,28 @@
+import imageZanzibar  from './zanzibar.jpg';
+import imageSerengeti  from './serengeti.jpg';
+import imageKilimanjaro  from './kilimanjaro.jpg';
+
+export const data = [
+    {
+        id: 1,
+        image: imageZanzibar,
+        name: "Zanzibar Island",
+        description: "The Zanzibar Archipelago, located in the Indian Ocean 15 miles off the coast of Tanzania, is a breathtaking spot to escape from the world. You’ll enjoy clear, turquoise-blue water; shallow sandbars perfect for wading; and many small, nearly deserted islands virtually unvisited by tourists. Explore the World Heritage Site of Stone Town, Zanzibar City’s old quarter. Or just go beach to beach between tiny fishing villages—each one's better than the next."
+    },
+
+    {
+        id: 2,
+        image: imageSerengeti,
+        name: "Serengeti National Park",
+        description: "The Serengeti is inarguably Tanzania’s best national park when it comes to wildlife; abundant with our planet’s most exotic animals (including elephants, giraffes, leopards, lions and buffalo), a visit to the Serengeti is nothing short of a magical experience. But what the park is undoubtedly best known for is the annual wildebeest migration. Visitors can witness the sound and sight of over six million wildebeest, zebra and gazelle scampering through the open plains in a hurry to find food and grazing, providing one of the most exhilarating game viewing experiences in the whole of Africa."
+    },
+
+    {
+        id: 3,
+        image: imageKilimanjaro,
+        name: "Kilimanjaro National Park",
+        description: "At 5,895 m, Kilimanjaro is the highest point in Africa. This volcanic massif stands in splendid isolation above the surrounding plains, with its snowy peak looming over the savannah. The mountain is encircled by mountain forest. Numerous mammals, many of them endangered species, live in the park."
+    }
+]
+
+
